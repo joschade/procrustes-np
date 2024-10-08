c38cf68b79489a146c0358da96acb78f232d55fc
Johannes Schade <jo.schade@posteo.de>
2024-10-08 14:14:51 +0200

    centering optionalized

--- procrustes_np.py
+++ procrustes_np.py
@@ -1,23 +1,30 @@
 from numpy import matmul, mean, array, shape
 from numpy.linalg import svd, det
 
-def orthogonal_procrustes(A: array, B: array, reflection = False) -> array:
+def orthogonal_procrustes(A: array, B: array, centering = True, reflection = False) -> array:
     """
     :param A: matrix to be matched on B by rigid motion
     :param B: matrix to be matched by A
+    :param centering: allow centering of A and B
+    :param reflection: allow reflection of Omega
     :return Omega: rigid motion matrix Omega such that Omega*A =~ B
     """
     assert A.shape == B.shape, "arrays A and B must be of equal shape"
 
-    # centering
-    a = mean(A, axis=1)
-    b = mean(B, axis=1)
+    if centering:
+        # centering
+        a = mean(A, axis=1)
+        b = mean(B, axis=1)
 
-    A_centered = (A.transpose() - a).transpose()
-    B_centered = (B.transpose() - b).transpose()
+        A_svd = (A.transpose() - a).transpose()
+        B_svd = (B.transpose() - b).transpose()
+
+    else:
+        A_svd = A
+        B_svd = B
 
     # perform SVD
-    U, _, Vh = svd(matmul(B_centered, A_centered.transpose()))
+    U, _, Vh = svd(matmul(B_svd, A_svd.transpose()))
 
     # make sure rigid motion (no reflection):
     if not reflection:
