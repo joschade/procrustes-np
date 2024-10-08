25564d9801d94a3618c106fd2a4e77b15dcabd14
Johannes Schade <jo.schade@posteo.de>
2024-10-08 10:54:24 +0200

    add comments, rm non-function items

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import numpy as np
 
-A = np.random.random(size = (3,5))
-B = np.random.random(size = (3,5))
-U, Sigma , Vh = np.linalg.svd(B, full_matrices=False)
-OmegaA = np.matmul(U, Vh)
-Omega = np.matmul(OmegaA, np.linalg.pinv(A))
-
-
 def orthogonal_procrustes(A: np.array, B: np.array) -> np.array:
     """
     :param A: matrix to be matched on B by rigid motion
     :param B: matrix to be matched by A
     :return: matrix Omega such that Omega*A =~ B
     """
+
+    # perform SVD
     U, Sigma, Vh = np.linalg.svd(B, compute_uv=True)
+    # Compute Omega * A (== U*Vh)
     OmegaA = np.matmul(U, Vh)
+
+    #compute Omega * A * pseudoInverse(A) (==Omega)
     Omega = np.matmul(OmegaA, np.linalg.pinv(A))
 
     return Omega
