4dbc16236dec963c485897d60ad2ed0147a27a78
Johannes Schade <jo.schade@posteo.de>
2024-10-08 11:49:31 +0200

    fix reflection

---
+++ procrustes_np.py
@@ -0,0 +1,28 @@
+from numpy import matmul, mean, array
+from numpy.linalg import svd,  det
+
+def orthogonal_procrustes(A: array, B: array, reflection = False) -> array:
+    """
+    :param A: matrix to be matched on B by rigid motion
+    :param B: matrix to be matched by A
+    :return Omega: rigid motion matrix Omega such that Omega*A =~ B
+    """
+    # centering
+    a = mean(A, axis=1)
+    b = mean(B, axis=1)
+
+    A_centered = (A.transpose() - a).transpose()
+    B_centered = (B.transpose() - b).transpose()
+
+    # perform SVD
+    U, _, Vh = svd(matmul(B_centered, A_centered.transpose()))
+
+    # make sure rigid motion (no reflection):
+    if not reflection:
+        if det(Vh) < 0.: Vh[0] = -Vh[0]
+
+    # compute Omega
+    Omega = matmul(U, Vh)
+
+    return Omega
+
